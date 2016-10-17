794114aef6655e7c83788ebde23c851a7b4fa1e5
Morgan Shattuck <mshattuck88@gmail.com>
2016-10-17 13:19:06 -0400

    Edited to include Giphy

--- final.py
+++ final.py
@@ -9,8 +9,13 @@ def index():
 
 @app.route('/results')
 def results():
+    g = giphypop.Giphy()
     gif = request.values.get('gif')
-    return render_template('results.html')
+    results = g.search(gif)
+    for result in results:
+        media_url = result.media_url
+        result_url = result.url
+    return render_template('results.html', gif=gif, results=results, media_url=media_url, result_url=result_url)
 
 @app.route('/about')
 def about():
